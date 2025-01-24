71331de869ae8288a57cac3e3496778de2a5ba10
Dmytro Kryvoruchenko <84386380+DmytroKryvoruchenko@users.noreply.github.com>
2025-01-24 15:29:33 +0200

    Bug/3 test bug 3 (#3)
    * fix(scope1): missing label 1
    
    1 Missing label
    
    Related work items: #3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,3 +13,7 @@ Console.WriteLine("Hello, World 5!");
 Console.WriteLine("Hello, World 6!");
 
 Console.WriteLine("Hello, World 7!");
+
+Console.WriteLine("Hello, World 8!");
+
+Console.WriteLine("Hello, World 9!");
